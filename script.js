7973aa7afe28a7e124b3d5bfc82296a52382cb91
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-05-03 11:39:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function updateClock() {
+    const now = new Date();
+    const h = String(now.getHours()).padStart(2, '0');
+    const m = String(now.getMinutes()).padStart(2, '0');
+    const s = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('hours').textContent = h;
+    document.getElementById('minutes').textContent = m;
+    document.getElementById('seconds').textContent = s;
+  }
+
+  setInterval(updateClock, 1000);
+  updateClock(); // запускаем сразу
